882e0ef6bd65a3daca75c8639ab10f9d91081bdc
Federico Fernandez <134979454+FernandezFederico@users.noreply.github.com>
2024-07-01 06:32:14 -0300

    feat(controllers): getCartByUserId

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -1,16 +1,16 @@
 import { CartModel } from "../models/CartModel.js";
 
-
 export const getCart = async (req, res) => {
-  try { 
+  try {
     const { id } = req.params;
     const cart = await CartModel.findById(id);
-    if (!cart) return res.status(404).json({ message: `Cart ID: '${id}' Not found` });
+    if (!cart)
+      return res.status(404).json({ message: `Cart ID: '${id}' Not found` });
     res.status(200).json(cart);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
-}
+};
 
 export const createCart = async (req, res) => {
   try {
@@ -19,29 +19,41 @@ export const createCart = async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
-}
+};
 
 export const updateCart = async (req, res) => {
   try {
     const { id } = req.params;
-    const cart = await CartModel.findByIdAndUpdate(
-      { _id: id },
-      req.body,
-      { new: true }
-    );
+    const cart = await CartModel.findByIdAndUpdate({ _id: id }, req.body, {
+      new: true,
+    });
     res.status(200).json(cart);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
-}
+};
 
 export const deleteCart = async (req, res) => {
   try {
     const { id } = req.params;
     const cart = await CartModel.findByIdAndDelete(id);
-    if (!cart) return res.status(404).json({ message: `Cart ID: '${id}' Not found` });
+    if (!cart)
+      return res.status(404).json({ message: `Cart ID: '${id}' Not found` });
     res.status(200).json("Cart deleted successfully");
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+};
+
+export const getCartByUserId = async ( req , res ) => {
+  try{
+    const {userId} = req.params;
+    const cartItems = await CartModel.find({ userId: userId })
+    if(cartItems.length === 0){
+      return res.status(404).json ({ message: `No cart items found for user ID: '${userId}'`})
+    }
+    res.status(200).json(cartItems)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
   }
 }
--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { getCart, createCart, updateCart, deleteCart } from "../controllers/CartController.js";
+import { getCart, createCart, updateCart, deleteCart, getCartByUserId } from "../controllers/CartController.js";
 const router = express.Router()
 
 router.get("/:id", getCart)
 router.post("/", createCart)
 router.put("/:id", updateCart)
 router.delete("/:id", deleteCart)
-
+router.get("/user/:userId", getCartByUserId)
 export default router
